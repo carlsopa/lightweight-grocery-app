d3828d311cfa89125128629c60fbe343edae7c56
carlsopa <carlsonpar@gmail.com>
2019-05-07 20:02:17 -0400

    php change
    update to the php file

--- Grocery2/FrontEnd/Javascript/grocery2_javascript.js
+++ Grocery2/FrontEnd/Javascript/grocery2_javascript.js
@@ -65,7 +65,7 @@ function NavButtons() {
 }
 function updateList(type,data) {
     var request = new XMLHttpRequest();
-    request.open('POST', '../BackEnd/php/1.php');
+    request.open('POST', '../BackEnd/php/grocerylist2_php.php');
     request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     console.log(listIndex);
         sendData = [{ 'type': type, 'id': listIndex, 'data': data }];
